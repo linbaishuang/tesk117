0064a0c0f2df4622e3a932735dfa4a6f99040dd3
linbaishuang <1765259718@qq.com>
2017-12-22 11:42:53 +0800

    修改

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,45 @@
-// $(".but").click(function () {
-//     alert("dsds");
-//     $(".show").show();
-// });
+$(function () {
+    $(".but").click(function () {
+        $(".show").show();
+    });
+    $(".hide").click(function () {
+        $(".show").hide();
+    });
+    var append = "";
+    var i = 0;
+    $(".sure").click(function () {
+        var val = $("input[name = 'cho']:checked").val();
+        if (val === '文本') {
+            for (i; ;) {
+                append = "<div>" +
+                    "<input type='text' value='文本' class='addword" + i + "'/>" +
+                    "<button class='deleword" + i + "' name='dele'>删除</button>" +
+                    "</div>";
+                i++;
+                break;
+            }
+            $("#container").append(append);
+        } else if (val === '日期') {
+            for (i; ;) {
+                append = "<div>" +
+                    "<input type='text' value='日期' class='addtime" + i + "'/>" +
+                    "<button class='deletime" + i + "' name='dele'>删除</button>" +
+                    "</div>";
+                i++;
+                break;
+            }
+            $("#container").append(append);
+        }
+        $("button[name = 'dele']").click(function () {
+            var deleword = this.className;
+            $("." + deleword + "").hide();
+            $("." + deleword + "").siblings().hide();
+        });
+    })
+    $(".see").click(function () {
+        $(this).html("编辑");
+        $(".but").hide();
+        $("button[name = 'dele']").hide();
+        $(".show").hide();
+    })
+})
